fff933b7378e805febba9e285b7641a592b52a7f
Robbie Bardijn <robbie.bardijn@gmail.com>
2012-10-11 15:33:56 +0200

    aliasing oil commands

--- oil/aliases.sh
+++ oil/aliases.sh
@@ -1,7 +1,21 @@
 # GENERATING
+alias ocp='oil create blog'
+alias ogmo='oil g model'
+alias ogmi='oil g migration'
+alias ogcont='oil g controller'
+alias ogv='oil g views'
+alias ogconf='oil g config'
+alias ogs='oil g scaffold'
 
 # REFINING
+alias orm='oil r migrate'
+alias ormup='oil r migrate:up'
+alias ormdown='oil r migrate:down'
+alias or='oil r'
 
 # TESTING
 
 # OTHER
+alias ocons='oil console'
+alias opi='oil package install'
+alias opi='oil package uninstall'
