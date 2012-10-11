bff87f4693742319c63e2800e8d236041e152382
Robbie Bardijn <robbie.bardijn@gmail.com>
2012-10-11 16:56:19 +0200

    fixed bug in git aliases

--- git/aliases.sh
+++ git/aliases.sh
@@ -10,21 +10,23 @@ alias glui='gitk --all'
 # SETTING TO AND REMOVING FROM STAGING AREA
 alias ga='git add'
 alias gaall='git add *'
-alias grm='git remove'
-alias grmall='git remove --cached *'
+alias grm='git rm'
+alias grmall='git rm --cached *'
 
 # (LOCAL REPO CHANGING) COMMITTING BRANCHING DIFFING & CHECKOUTING
 alias gc='git commit'
 alias gb='git branch'
+alias gm='git merge'
 alias gco='git checkout'
 alias gd='git diff'
 
 # REMOTE
 alias gpull='git pull'
 alias gpush='git push'
+alias gf='git fetch'
 alias gcl='git clone'
 alias gclr='git clone --recursive'
 
 # ARCHIVE EXPORT AND PULL ALL REPOS
 alias pall='find ~/Documents/REPO -type d -name .git | xargs -n 1 dirname | sort | while read line; do echo $line && cd $line && git pull; done'
-alias gitam='git archive master | tar -x -C $@'
+alias gam='git archive master | tar -x -C $@'
