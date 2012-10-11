f6bb6d850fd1fb0245278f0fe0e6b596e7042c37
Robbie Bardijn <robbie.bardijn@gmail.com>
2012-10-11 14:41:19 +0200

    adding git aliasses and system aliasses

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,8 +1,30 @@
-alias gst='echo "» git status"; git status'
-alias gp='echo "» git push"; git push "$@"'
-alias gph='echo "» git push heroku heroku:master"; git push heroku heroku:master'
-alias gcam='echo "» git commit -a -m"; git commit -a -m "$@"'
-alias gall='echo "» git add ."; git add .'
-alias gca='echo "» git commit -a"; git commit -a'
+# STATUSSES AND LOGS
+alias gst='git status'
+alias gl='git log'
+alias gls='git log --stat'
+alias glo='git log --oneline'
+alias glg='git log --graph'
+alias glogad='git log --oneline --graph --all --decorate'
+alias glui='gitk --all'
 
-alias glog='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
+# SETTING TO AND REMOVING FROM STAGING AREA
+alias ga='git add'
+alias gaall='git add *'
+alias grm='git remove'
+alias grmall='git remove --cached *'
+
+# (LOCAL REPO CHANGING) COMMITTING BRANCHING DIFFING & CHECKOUTING
+alias gc='git commit'
+alias gb='git branch'
+alias gco='git checkout'
+alias gd='git diff'
+
+# REMOTE
+alias gpull='git pull'
+alias gpush='git push'
+alias gcl='git clone'
+alias gclr='git clone --recursive'
+
+# ARCHIVE EXPORT AND PULL ALL REPOS
+alias pall='find ~/Documents/REPO -type d -name .git | xargs -n 1 dirname | sort | while read line; do echo $line && cd $line && git pull; done'
+alias gitam='git archive master | tar -x -C $@'
--- system/aliases.sh
+++ system/aliases.sh
@@ -1,41 +1,45 @@
 # paths
 ################################################################################################################
-alias projects='cd ~/Doucments/PROJECTS/'
+alias projects='cd ~/Documents/PROJECTS/'
 alias own='cd ~/Documents/OWN_PROJECTS/'
 alias temp='cd ~/Documents/TEMP/'
 alias expo='cd ~/Documents/EXPORTS/'
 alias files='cd ~/Documents/PROX_FILES/'
 alias repo='cd ~/Documents/REPO/'
 
-
+################################################################################################################
+alias mkassets='mkdir css img font download js img/layout img/icon js/lib css/lib'
+################################################################################################################
 alias reload!='. ~/.bash_profile'
 alias termieter='cd ~/.termieter'
-alias vim='cd ~/.vim'
-################################################################################################################
 
+################################################################################################################
+alias vimconf='cd ~/.vim'
+alias v='vim .'
+
+################################################################################################################
 alias ..='cd ..'
 alias ...='cd .. ; cd ..'
-
 alias ls='ls $LS_OPT' # long list, excludes dot files
 alias ll='ls $LS_OPT -GlA' # long list all, includes dot files
-alias l='ls $LS_OPT -AFo'
+alias l='ls $LS_OPT -loAF'
+alias cwd='pwd | pbcopy' # copy current working directory to clipboard
 
+################################################################################################################
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'" # Get readable list of network IPs
 alias ip='dig +short myip.opendns.com @resolver1.opendns.com' # my ip
 alias flushdns='echo "» dscacheutil -flushcache"; dscacheutil -flushcache' # Flush DNS cache
 
+################################################################################################################
 alias gzip='echo "» gzip -9n"; gzip -9n' # set strongest compression level as ‘default’ for gzip
 alias ping='echo "» pint -c 5"; ping -c 5' # ping 5 times ‘by default’
-
-alias cwd='pwd | pbcopy' # copy current working directory to clipboard
-
 alias hosts='sudo vim /private/etc/hosts'
 
+################################################################################################################
 alias hist='history | grep "$@"'
 alias hist-sort='history | cut -c 8- | sort | uniq -c | sort -rn'
 
-alias v='vim .'
-
+################################################################################################################
 # Clear temp files for a faster Terminal
 alias fasterfaster='sudo rm -rf /private/var/log/asl/*; sudo rm -rf /var/mail/*'
 
@@ -48,3 +52,5 @@ alias trash='mv "$@" ~/.Trash/'
 # SSH background color
 alias ssh='echo "» ssh-host-color-iterm"; ~/.termieter/system/functions/ssh-host-color-iterm'
 
+################################################################################################################
+alias codekit='open -a ~/Applications/CodeKit.app $(pwd)';
